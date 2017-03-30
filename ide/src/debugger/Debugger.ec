LesterLogic/ecere-sdk

commit 9cc561eb3d827cf3876465c9512298bde193b43c

ide/debugger/watches: Fixes to properly display GeoExtent - extent, extent.ll, extent.ur, extent.ll.lat, extent.ur.lon now all work properly

ide/src/debugger/Debugger.ec
@@ -2705,13 +2705,6 @@ class Debugger
                else if(exp.expType && exp.expType.kind == classType && exp.expType._class && exp.expType._class.registered && exp.expType._class.registered.type == structClass && exp.hasAddress)
                {
                   Class c = exp.expType._class.registered;
-                  char structString[1024];
-                  strcpy(structString, "*(struct ");
-                  FullClassNameCat(structString, c.fullName, false);
-                  strcat(structString, " *)");
-                  strcatf(structString, "0x%p", exp.address);
-                  GDBFallBack(exp, structString);
-                  /*
                   byte * data = GdbReadMemory(exp.address, c.structSize);
                   if(data)
                   {
@@ -2723,11 +2716,19 @@ class Debugger
                         FreeExpContents(exp);
                         exp.type = constantExp;
                         exp.isConstant = true;
-                        exp.constant = CopyString(s);
+                        exp.constant = PrintString("{ ", s, " }");
                      }
                      delete data;
                   }
-                  */
+                  else
+                  {
+                     char structString[1024];
+                     strcpy(structString, "*(struct ");
+                     FullClassNameCat(structString, c.fullName, false);
+                     strcat(structString, " *)");
+                     strcatf(structString, "0x%p", exp.address);
+                     GDBFallBack(exp, structString);
+                  }
                }
 
                if(ExpressionIsError(exp) && exp.type != functionCallErrorExp)

ide/src/debugger/debugTools.ec
@@ -2300,6 +2300,11 @@ void DebugComputeExpression(Expression exp)
                                     exp.expType.refCount++;
                                     if(exp.expType.kind == classType && exp.expType._class && exp.expType._class.registered && exp.expType._class.registered.type == unitClass)
                                     {
+                                       if(expNew.type == opExp)
+                                       {
+                                          if(expNew.op.exp1) ProcessExpressionType(expNew.op.exp1);
+                                          if(expNew.op.exp2) ProcessExpressionType(expNew.op.exp2);
+                                       }
                                        expNew.expType = exp.expType;
                                        exp.expType.refCount++;
                                     }

commit caa709b3181efbff0aeae4ee16216c58f1fae6ed

ide/debugger: Resolved crashes caused by 9cc561eb3d827cf3876465c9512298bde193b43c - Avoiding to stringify instances containing debugged process memory

ide/src/debugger/Debugger.ec
@@ -2705,7 +2705,25 @@ class Debugger
                else if(exp.expType && exp.expType.kind == classType && exp.expType._class && exp.expType._class.registered && exp.expType._class.registered.type == structClass && exp.hasAddress)
                {
                   Class c = exp.expType._class.registered;
-                  byte * data = GdbReadMemory(exp.address, c.structSize);
+                  DataMember m;
+                  bool getString = true;
+                  byte * data = null;
+                  for(m = c.membersAndProperties.first; m; m = m.next)
+                  {
+                     if(!m.isProperty && m.type == normalMember)
+                     {
+                        Class type = m.dataTypeClass;
+                        if(!type)
+                           type = m.dataTypeClass = eSystem_FindClass(c.module, m.dataTypeString);
+                        if(!type || type.type == noHeadClass || type.type == normalClass)
+                        {
+                           // Avoid trying to stringify types referencing debugged process memory
+                           getString = false;
+                           break;
+                        }
+                     }
+                  }
+                  data = getString ? GdbReadMemory(exp.address, c.structSize) : null;
                   if(data)
                   {
                      char tmp[4096];